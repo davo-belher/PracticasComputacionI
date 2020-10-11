49462da0f1a012db05ae3e21cfd0e631f8dab6ee
davo-belher <davo.belher@comunidad.unam.mx>
2020-10-11 18:58:31 -0500

    funciona la calculadora utilizando switch statements pero no funciona el %

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -1,6 +1,46 @@
+/*===============================
+ * Autor:Omar Beltran
+ * davo.belher@comunidad.unam.mx
+ * 11/10/2020
+ ===============================*/
+
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    //inicializa los valores
+    float x1,x2,a;
+    char act;
+
+    // Pide el imput de los usuarios
+    cout << "Ingrese primer valor: ";
+    cin >> x1;
+
+    cout << "Ingrese operacion (+,-,*,/,%): ";
+    cin >> act;
+
+    cout << "Ingrese segundo valor: ";
+    cin >> x2;
+
+    switch(act){
+        case '+':
+            cout << x1+x2 << endl;
+            break;
+        case '-':
+            cout << x1-x2 << endl;
+            break;
+        case '*':
+            cout << x1 * x2 << endl;
+            break;
+        case '/':
+            cout << x1 / x2 << endl;
+            break;
+
+        default:
+            cout << "operacion invalida";
+            break;
+
+
+    }
+return 0;
 }
