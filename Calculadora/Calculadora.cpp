ad962914387cb9ede8b1a39f4c5a5301dae1a2f8
davo-belher <davo.belher@comunidad.unam.mx>
2020-10-11 20:44:16 -0500

    agregado comentarios para la calculadora

--- Calculadora/Calculadora.cpp
+++ Calculadora/Calculadora.cpp
@@ -23,6 +23,13 @@ int main() {
     cout << "Ingrese segundo valor: ";
     cin >> x2;
 
+    /*
+     * utilice un switch en vez de varios if statements
+     * dependiendo de lo que el usuario elige  es lo que se realiza
+     * sigue la misma logica que los if statements
+     * pero no realiza todas las acciones de comparacion para llegar al codigo apropiado
+     */
+
     switch(act){
         case '+':
             cout << x1+x2 << endl;
@@ -36,6 +43,10 @@ int main() {
         case '/':
             cout << x1 / x2 << endl;
             break;
+            /* estaba teniendo problemas con los modulos, daba un codigo de error
+             * no se podia realizar la accion modulo con los operadores binairos si se analizaban floats
+             * estaba entre utilizar int o buscar otra forma de hacerlo funcionar
+             */
         case '%':
             a= fmod(x1,x2);
             cout << a << endl;
@@ -43,7 +54,6 @@ int main() {
         default:
             cout << "operacion invalida";
             break;
-
 
     }
 return 0;
