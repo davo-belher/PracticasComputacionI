729ef650dc16abf532c01a61b64be1f827ae2c13
davo-belher <davo.belher@comunidad.unam.mx>
2020-11-04 10:29:06 -0600

    crear punto.cpp y punto.h al igual que la carpeta

---
+++ Puntos/Punto.h
@@ -0,0 +1,22 @@
+//
+// Created by David Omar on 31/10/2020.
+//
+
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+
+#include <iostream>
+
+using namespace std;
+class Punto2D {
+
+public:
+    void PedirValores(float x,float y);
+    Punto2D()
+    void SetPosicion(float x, float y);
+
+
+private:
+
+}
+#endif //PUNTOS_PUNTO_H
---
+++ Puntos/cmake-build-debug/CMakeFiles/Puntos.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Puntos.dir/Punto.cpp.obj"
+  "Puntos.exe"
+  "Puntos.exe.manifest"
+  "Puntos.pdb"
+  "libPuntos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Puntos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
