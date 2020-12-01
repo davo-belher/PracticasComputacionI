9157581f73942ce40dba7cc33358f73ca12e471b
davo-belher <davo.belher@comunidad.unam.mx>
2020-12-01 11:14:50 -0600

    crear carpeta y practica terminada

---
+++ Almacen/Bodega.h
@@ -0,0 +1,28 @@
+//
+// Created by David Omar on 01/12/2020.
+//
+
+
+#ifndef ALMACEN_BODEGA_H
+#define ALMACEN_BODEGA_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Bodega
+{
+private:
+    string nombre;
+    vector<string*> articulos;
+public:
+    Bodega() { nombre = ""; }
+    Bodega(string nom) { nombre = nom; }
+    string ObtenerNombre() { return nombre; }
+    bool AgregarArticulo(string* artPtr);
+    string ObtenerInventario();
+};
+
+
+#endif // !ALMACEN_BODEGA_H
---
+++ Almacen/cmake-build-debug/CMakeFiles/Almacen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Almacen.exe"
+  "Almacen.exe.manifest"
+  "Almacen.pdb"
+  "CMakeFiles/Almacen.dir/Almacen.cpp.obj"
+  "CMakeFiles/Almacen.dir/Bodega.cpp.obj"
+  "libAlmacen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Almacen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
